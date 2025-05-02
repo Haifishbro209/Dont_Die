8b07192cdb976373bf3015ee3326106b591ea54a
haifishbro209 <kaloyannikolov016@gmail.com>
2025-05-02 22:39:19 +0200

    .

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 from flask import *
 from sqlalchemy import *
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    
+    return render_template("landingpage.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
