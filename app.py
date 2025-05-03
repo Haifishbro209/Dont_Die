f7e614db944eeb68e6f9b7e29425d28c1a38e421
haifishbro209 <kaloyannikolov016@gmail.com>
2025-05-03 11:35:48 +0200

    .

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import *
 from sqlalchemy import *
-#from database import *
+from database import *
 
 app = Flask(__name__)
 
@@ -9,9 +9,19 @@ def index():
     
     return render_template("landingpage.html")
 
+@app.route("/health/<username>")
+def sign_up(username):
+    return render_template("health.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 @app.route("/app/<user_name>")
 def overview(user_name):
-    #add_user(user_name)
+    add_user(user_name)
     return render_template("app.html")
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
 
-DATABASE_URL = "sqlite:///users.db"
+DATABASE_URL = "sqlite:///db.db"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
@@ -14,71 +14,92 @@ class User(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String, nullable=False, unique=True)
 
-class Stats(Base):
-    __tablename__ = 'stats'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-   
-
 class Day(Base):
     __tablename__ = "days"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
 
     date = Column(Date, default=datetime.date.today)
-    score = Column(Integer)
+    score = Column(Integer, default=0)
 
-    KFA = Column(Integer)
-    weight_in_kg = Column(Integer)
-    height_in_cm = Column(Integer)
-    BMI = Column(Integer)
-    chill_HR = Column(Integer)
-    systole = Column(Integer)
-    dyastole = Column(Integer)
+    KFA = Column(Integer, default=0)
+    weight_in_kg = Column(Integer, default=0)
+    height_in_cm = Column(Integer, default=0)
+    BMI = Column(Integer, default=0)
+    chill_HR = Column(Integer, default=0)
+    systole = Column(Integer, default=0)
+    dyastole = Column(Integer, default=0)
 
-    Food_score = Column(Integer)
-    Food_kcal = Column(Integer)
-    Food_protein = Column(Integer)
-    Food_junk = Column(Boolean)
-    Food_suggar = Column(Integer)
-    Food_notes_combined = Column(String)
+    Food_score = Column(Integer, default=0)
+    Food_kcal = Column(Integer, default=0)
+    Food_protein = Column(Integer, default=0)
+    Food_junk = Column(Boolean, default=False)
+    Food_suggar = Column(Integer, default=0)
+    Food_notes_combined = Column(String, default="")
 
-    Sport_cardio = Column(Boolean)
-    Sport_strenght = Column(Boolean)
-    Sport_minutes = Column(Integer)
-    Sport_steps = Column(Integer)
+    Sport_cardio = Column(Boolean, default=False)
+    Sport_strenght = Column(Boolean, default=False)
+    Sport_minutes = Column(Integer, default=0)
+    Sport_steps = Column(Integer, default=0)
 
-    Sleep_hours = Column(Float)
-    Sleep_mood = Column(Integer)
-    Sleep_continuity = Column(Boolean)
+    Sleep_hours = Column(Float, default=0.0)
+    Sleep_mood = Column(Integer, default=0)
+    Sleep_continuity = Column(Boolean, default=False)
 
 class FoodEntry(Base):
+    __tablename__ = "food_entries"
     id = Column(Integer, primary_key=True, autoincrement=True)
     day_id = Column(Integer, ForeignKey('days.id'), nullable=False)
-    kcal = Column(Integer)
-    junk = Column(Boolean)
-    suggar = Column(Integer)
-    note = Column(String)
-    protein = Column(Integer)
+    kcal = Column(Integer, default=0)
+    junk = Column(Boolean, default=False)
+    suggar = Column(Integer, default=0)
+    note = Column(String, default="")
+    protein = Column(Integer, default=0)
 
 class SportEntry(Base):
+    __tablename__ = "sport_entries"
     id = Column(Integer, primary_key=True, autoincrement=True)
     day_id = Column(Integer, ForeignKey('days.id'), nullable=False)
-    cardio = Column(Boolean)
-    minutes = Column(Integer)
-    strength = Column(Boolean)
-    steps= Column(Integer)
-
-
-
-
-
-
+    cardio = Column(Boolean, default=False)
+    minutes = Column(Integer, default=0)
+    strength = Column(Boolean, default=False)
+    steps = Column(Integer, default=0)
 
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+def check_day(user_id):
+    date = datetime.date.today()
+    day_exists_id = session.query(Day.id).filter(
+        Day.date == date,
+        Day.user_id == user_id
+    ).scalar()
+    if day_exists_id:
+        return day_exists_id
+    else:
+        new_day = Day(user_id=user_id, date=date)
+        session.add(new_day)
+        session.commit()
+        return new_day.id
+    
+
+def add_sport_Entry(user_id,cardio = False,strength= False,minutes= 0,steps= 0):
+    day_id = check_day(user_id)
+    new_entry = SportEntry(day_id = day_id,cardio = cardio, strength = strength,minutes = minutes,steps= steps)
+    session.add(new_entry)
+    day = session.query(Day).filter(Day.id == day_id).first()
+
+    day.Sport_minutes += minutes
+    day.Sport_steps += steps
+    if cardio:
+        day.Sport_cardio = True
+    if strength:
+        day.Sport_strenght = True
+    
+    session.add(day)
+    session.commit()
 
 def add_user(username):
     user = session.query(User).filter(User.username == username).first()
